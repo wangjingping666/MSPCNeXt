78c7a1e949e1e39f6eed6ca6e5fcc8809d919293
wangjingping666 <120068071+wangjingping666@users.noreply.github.com>
2024-03-07 17:01:11 +0800

    Update convnext.py

--- convnext.py
+++ convnext.py
@@ -4,18 +4,17 @@ import torch.nn.functional as F
 from timm.models.layers import trunc_normal_, DropPath
 from timm.models.registry import register_model
 from thop import profile
-str_model = ""
+
 class Block(nn.Module):
     def __init__(self, dim,drop_path=0., layer_scale_init_value=1e-6):
         super(Block, self).__init__()
-        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim)  # depthwise conv
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim)
         self.norm = nn.LayerNorm(dim, eps=1e-6)
         self.pwconv1 = nn.Linear(dim, 4 * dim)
         self.act = nn.GELU()
         self.pwconv2 = nn.Linear(4 * dim, dim)
         self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
                                   requires_grad=True) if layer_scale_init_value > 0 else None
-
         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
     def forward(self, x):
@@ -104,7 +103,7 @@ class LayerNorm(nn.Module):
             return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
         elif self.data_format == "channels_first":
             u = x.mean(1, keepdim=True)
-            s = (x - u).pow(2).mean(1, keepdim=True)  # 方差
+            s = (x - u).pow(2).mean(1, keepdim=True)
             x = (x - u) / torch.sqrt(s + self.eps)
             x = self.weight[:, None, None] * x + self.bias[:, None, None]
             return x
@@ -132,5 +131,5 @@ if __name__ == '__main__':
     print(outputs)
     flops, params = profile(model, (inputs,))
     print('flops: ', flops, 'params: ', params)
-    print("输入维度:", inputs.shape)
-    print("输出维度:", outputs.shape)
+    print("input dimension:", inputs.shape)
+    print("output dimension:", outputs.shape)
