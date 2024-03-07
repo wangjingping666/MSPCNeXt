ea17101053119097e6a8ea80247b5314c234108c
wangjingping666 <120068071+wangjingping666@users.noreply.github.com>
2024-03-07 16:49:34 +0800

    Update convnext_v2.py

--- convnext_v2.py
+++ convnext_v2.py
@@ -1,96 +1,339 @@
-import os
-import sys
-import json
-import time
 import torch
-from torch.optim.lr_scheduler import CosineAnnealingLR, CosineAnnealingWarmRestarts, StepLR, OneCycleLR
-import torch_optimizer as optim
-from torchvision import transforms, datasets
 import torch.nn as nn
-from utils import train_and_val, plot_acc, plot_loss
-import numpy as np
-from torch.utils.data import DataLoader, random_split
-from torch.optim.lr_scheduler import ReduceLROnPlateau
-import sys
-import torch
-from torch.utils.data import dataloader
-from torch.multiprocessing import reductions
-from multiprocessing.reduction import ForkingPickler
-from torch_ema import ExponentialMovingAverage
-from resnet50 import resnet50
-from MSPCNeXt_v1 import mspcnext_v1
-from MSPCNeXt_v2 import mspcnext_v2
-
-default_collate_func = dataloader.default_collate
+import torch.nn.functional as F
+from timm.models.layers import trunc_normal_, DropPath
+from timm.models.registry import register_model
+from thop import profile
 
 
-def default_collate_override(batch):
-    dataloader._use_shared_memory = False
-    return default_collate_func(batch)
+"""
+Designed a BN module with BatchNorm2d and GELU
+"""
+class BN(nn.Module):
+    def __init__(self, out_channels,**kwargs):
+        super(BN, self).__init__()
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.00001)
+        self.act = nn.GELU()
+    def forward(self, x):
+        x = self.bn(x)
+        x = self.act(x)
+        return x
 
 
-setattr(dataloader, 'default_collate', default_collate_override)
+"""
+The 3*3 convolution block for Stage1. Adapted from ConvNeXt_Block.
+"""
+class Block_3(nn.Module):
+    def __init__(self, dim,drop_path=0.6, layer_scale_init_value=1e-6):
+        super(Block_3, self).__init__()
+        self.dwconv3_3_a1 = nn.Conv2d(dim, 96, kernel_size=(3,1), padding=(0,1), groups=96)
+        self.dwconv3_3_a2 = nn.Conv2d(96, 96, kernel_size=(1,3), padding=(1,0), groups=96)
+        self.bn1 = BN(96, eps=0.00001)    #Each branch is immediately followed by a BN module.
+        self.pwconv1 = nn.Linear(dim, 4 * dim)
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
+                                  requires_grad=True) if layer_scale_init_value > 0 else None
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
 
-for t in torch._storage_classes:
-    if sys.version_info[0] == 2:
-        if t in ForkingPickler.dispatch:
-            del ForkingPickler.dispatch[t]
-    else:
-        if t in ForkingPickler._extra_reducers:
-            del ForkingPickler._extra_reducers[t]
+    def forward(self, x):
+        input = x
+        x = self.dwconv3_3_a1(x)
+        x = self.dwconv3_3_a2(x)
+        x = self.bn1(x)
+        x = x.permute(0, 2, 3, 1)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 3, 1, 2)
+        x = input + self.drop_path(x)
+        return x
+
+
+# class Block_5(nn.Module):
+#     def __init__(self, dim,drop_path=0.6, layer_scale_init_value=1e-6):
+#         super(Block_5, self).__init__()
+#         self.dwconv5_5_a1 = nn.Conv2d(dim, 96, kernel_size=(5,1), padding=1, groups=96)
+#         self.dwconv5_5_a2 = nn.Conv2d(96, 96, kernel_size=(1,5), padding=1, groups=96)
+#         self.bn2 = BN(96, eps=0.00001)
+#         self.pwconv1 = nn.Linear(dim, 4 * dim)
+#         self.act = nn.GELU()
+#         self.pwconv2 = nn.Linear(4 * dim, dim)
+#         self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
+#                                   requires_grad=True) if layer_scale_init_value > 0 else None
+#
+#         self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+#
+#     def forward(self, x):
+#         input = x
+#         x = self.dwconv5_5_a1(x)
+#         x = self.dwconv5_5_a2(x)
+#         x = self.bn2(x)
+#         x = x.permute(0, 2, 3, 1)
+#         x = self.pwconv1(x)
+#         x = self.act(x)
+#         x = self.pwconv2(x)
+#         if self.gamma is not None:
+#             x = self.gamma * x
+#         x = x.permute(0, 3, 1, 2)
+#         x = input + self.drop_path(x)
+#         return x
+
+"""
+The 7*7 convolution block for Stage1. Adapted from ConvNeXt_Block. Factorise the 7*7 convolution kernel in the original ConvNeXt_Block.
+"""
+class Block_7(nn.Module):
+    def __init__(self, dim,drop_path=0.6, layer_scale_init_value=1e-6):
+        super(Block_7, self).__init__()
+        self.dwconv7_7_a1 = nn.Conv2d(dim, 96, kernel_size=(7, 1), padding=1, groups=96)
+        self.dwconv7_7_a2 = nn.Conv2d(96, 96, kernel_size=(1, 7), padding=2, groups=96)
+        self.bn2 = BN(96, eps=0.00001)    #Each branch is immediately followed by a BN module.
+        self.pwconv1 = nn.Linear(96, 4 * dim)
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
+                                  requires_grad=True) if layer_scale_init_value > 0 else None
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv7_7_a1(x)
+        x = self.dwconv7_7_a2(x)
+        x = self.bn2(x)
+        x = x.permute(0, 2, 3, 1)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 3, 1, 2)
+        x = input + self.drop_path(x)
+        return x
+
+
+
+"""
+The mspcnext_v2_Block module. Contains 4 parallel branches, which are:1×1, 5×5, 9×9 and 11×11 convolutions.Large convolution kernel for factorisation.
+"""
+class mspcnext_v2_Block(nn.Module):
+    def __init__(self, dim, drop_path=0.6, layer_scale_init_value=1e-6):
+        super(mspcnext_v2_Block, self).__init__()
+        dim_d = int(dim / 4)
+        self.dwconv1_1_1 = nn.Conv2d(dim, dim_d, kernel_size=1)
+        self.bn1 = BN(dim_d, eps=0.00001)
+
+        # self.dwconv3_3_a = nn.Conv2d(dim_d, dim_d, kernel_size=3,padding=1,groups=dim_d) #, padding=3, groups=dim_d)
+        # self.bn3 = BN(dim_d, eps=0.00001)
+
+        # self.dwconv5_5_a = nn.Conv2d(dim_d, dim_d, kernel_size=5,padding=2,groups=dim_d) #, padding=3, groups=dim_d)
+        self.dwconv5_5_a1 = nn.Conv2d(dim, dim_d, kernel_size=(5, 1), padding=1, groups=dim_d)
+        self.dwconv5_5_a2 = nn.Conv2d(dim_d, dim_d, kernel_size=(1, 5), padding=1, groups=dim_d)
+        self.bn5 = BN(dim_d, eps=0.00001)     #Each branch is immediately followed by a BN module.
+
+        # self.dwconv7_7_a = nn.Conv2d(dim_d, dim_d, kernel_size=7, padding=3, groups=dim_d)
+        # self.dwconv7_7_a1 = nn.Conv2d(dim_d, dim_d, kernel_size=(7, 1), padding=1, groups=dim_d)
+        # self.dwconv7_7_a2 = nn.Conv2d(dim_d, dim_d, kernel_size=(1, 7), padding=2, groups=dim_d)
+        # self.bn7 = BN(dim_d, eps=0.00001)
+
+        # self.dwconv7_7_a = nn.Conv2d(dim_d, dim_d, kernel_size=9, padding=3, groups=dim_d)
+        self.dwconv9_9_a1 = nn.Conv2d(dim, dim_d, kernel_size=(9, 1), padding=2, groups=dim_d)
+        self.dwconv9_9_a2 = nn.Conv2d(dim_d, dim_d, kernel_size=(1, 9), padding=2, groups=dim_d)
+        self.bn9 = BN(dim_d, eps=0.00001)    #Each branch is immediately followed by a BN module.
+
+        self.dwconv11_11_b1 = nn.Conv2d(dim, dim_d, kernel_size=(11, 1), padding=2, groups=dim_d)
+        self.dwconv11_11_b2 = nn.Conv2d(dim_d, dim_d, kernel_size=(1, 11), padding=3, groups=dim_d)
+        self.bn11 = BN(dim_d, eps=0.00001)
+
+        self.pwconv1 = nn.Linear(dim, 4 * dim)
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(4 * dim, dim)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim)),
+                                  requires_grad=True) if layer_scale_init_value > 0 else None
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+
+        dwconv1_1_1 = self.dwconv1_1_1(x)
+        dwconv1_1_1 = self.bn1(dwconv1_1_1)
+
+        # dwconv3_3_a = self.dwconv3_3_a(x)
+        # dwconv3_3_a1 = self.bn1(dwconv3_3_a)
+
+        dwconv5_5_a1 = self.dwconv5_5_a1(x)
+        dwconv5_5_a2 = self.dwconv5_5_a2(dwconv5_5_a1)
+        dwconv5_5_a3 = self.bn5(dwconv5_5_a2)
+
+        # dwconv7_7_a1 = self.dwconv7_7_a1(x)
+        # dwconv7_7_a2 = self.dwconv7_7_a2(dwconv7_7_a1)
+        # dwconv7_7_a3 = self.bn7(dwconv7_7_a2)
+
+        dwconv9_9_a1 = self.dwconv9_9_a1(x)
+        dwconv9_9_a2 = self.dwconv9_9_a2(dwconv9_9_a1)
+        dwconv9_9_a3 = self.bn9(dwconv9_9_a2)
+
+        dwconv11_11_b1 = self.dwconv11_11_b1(x)
+        dwconv11_11_b3 = self.dwconv11_11_b2(dwconv11_11_b1)
+        dwconv11_11_b4 = self.bn11(dwconv11_11_b3)
+
+        x = [dwconv1_1_1, dwconv5_5_a3, dwconv9_9_a3, dwconv11_11_b4]
+
+        x = torch.cat(x, 1)
+        x = x.permute(0, 2, 3, 1)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.permute(0, 3, 1, 2)
+        x = input + self.drop_path(x)
+        return x
+
+
+class ConvNeXt(nn.Module):
+    def __init__(self, in_chans=3, num_classes=7, depths=[3, 3, 9, 3], dims=[96, 192, 384, 768],
+                 drop_path_rate=0.6, layer_scale_init_value=1e-6, head_init_scale=1.):
+        super(ConvNeXt, self).__init__()
+
+        self.downsample_layers = nn.ModuleList()
+        stem = nn.Sequential(
+            nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
+            LayerNorm(dims[0], eps=1e-6, data_format="channels_first")
+        )
+        self.downsample_layers.append(stem)
+
+        for i in range(3):
+            downsample_layer = nn.Sequential(
+                LayerNorm(dims[i], eps=1e-6,data_format="channels_first"),
+                nn.Conv2d(dims[i], dims[i + 1], kernel_size=2, stride=2)
+            )
+            self.downsample_layers.append(downsample_layer)
+
+        self.stages = nn.ModuleList()
+        dp_rates = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
+        cur = 0
+
+
+        """
+        Use customised Block modules in each Stage.
+        """
+        for i in range(4):
+            if i == 0:
+                stage = nn.Sequential(
+                    *[Block_3(dim=dims[i], drop_path=dp_rates[cur + j],
+                            layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+                )
+            elif i == 1:
+                 stage = nn.Sequential(
+                    *[Block_7(dim=dims[i], drop_path=dp_rates[cur + j],
+                                  layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+                )
+            elif i == 2:
+                stage = nn.Sequential(
+                    *[mspcnext_v2_Block(dim=dims[i],drop_path=dp_rates[cur + j],
+                              layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+                )
+            else:
+                stage = nn.Sequential(
+                    *[Block_7(dim=dims[i],drop_path=dp_rates[cur + j],
+                              layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+                )
+
+            """
+            The 4 Stages use the same mspcnext_v2_Block.
+            """
+            # stage = nn.Sequential(
+            #     *[mspcnext_v2_Block(dim=dims[i], drop_path=dp_rates[cur + j],
+            #                       layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+            # )
+
+            """
+            The first two Stages use a custom 7*7 convolution module. The last two Stages use mspcnext_v2_Block.
+            """
+            # if i <= 1:
+            #     stage = nn.Sequential(
+            #         *[Block_7(dim=dims[i],drop_path=dp_rates[cur + j],
+            #                   layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+            #     )
+            # else:
+            #     stage = nn.Sequential(
+            #         *[mspcnext_v2_Block(dim=dims[i],drop_path=dp_rates[cur + j],
+            #                   layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+            #     )
+
+
+            self.stages.append(stage)
+            cur += depths[i]
+
+        self.norm = nn.LayerNorm(dims[-1], eps=1e-6)
+        self.head = nn.Linear(dims[-1], num_classes)
+
+        self.apply(self._init_weights)
+        self.head.weight.data.mul_(head_init_scale)
+        self.head.bias.data.mul_(head_init_scale)
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv2d, nn.Linear)):
+            nn.init.trunc_normal_(m.weight, std=0.02)
+            nn.init.constant_(m.bias, 0)
+
+    def forward_features(self, x):
+        for i in range(4):
+            x = self.downsample_layers[i](x)
+            x = self.stages[i](x)
+        return self.norm(x.mean([-2, -1]))
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = self.head(x)
+        return x
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_last"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+        self.normalized_shape = (normalized_shape,)
+
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim=True)
+            s = (x - u).pow(2).mean(1, keepdim=True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+            return x
+
+
+model_urls = {
+    "convnext_tiny_1k": "https://dl.fbaipublicfiles.com/convnext/convnext_tiny_1k_224_ema.pth",
+}
+
+@register_model
+def mspcnext_v2(pretrained=False, in_22k=False, **kwargs):
+    model = ConvNeXt(depths=[3, 3, 9, 3], dims=[96, 192, 384, 768],**kwargs)
+    if pretrained:
+        url = model_urls['convnext_tiny_22k'] if in_22k else model_urls['convnext_tiny_1k']
+        checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
+        model.load_state_dict(checkpoint["model"])
+    return model
 
 if __name__ == '__main__':
-    # on_delete = nn.Module.CASCADE
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print("using {} device.".format(device))
-
-    if not os.path.exists('./weight'):
-        os.makedirs('./weight')
-
-    BATCH_SIZE = 64
-
-    data_transform = {
-        "train": transforms.Compose([transforms.Resize([224, 224]),
-                                     transforms.RandomResizedCrop((224, 224), scale=(0.9, 1.0), ratio=(1.0, 1.0)),
-                                     transforms.RandomHorizontalFlip(),
-                                     transforms.ToTensor(),
-                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])]),
-        "val": transforms.Compose([transforms.Resize([224, 224]),
-                                   transforms.ToTensor(),
-                                   transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])}
-
-    train_dataset = datasets.ImageFolder("Training set path",
-                                         transform=data_transform["train"])
-
-    val_percent = 0.2
-    n_val = int(len(train_dataset) * val_percent)
-    n_train = len(train_dataset) - n_val
-    train_set, val_set = random_split(train_dataset, (n_train, n_val), generator=torch.Generator())
-    train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=BATCH_SIZE, shuffle=True)
-    len_train = len(train_set)
-    val_loader = torch.utils.data.DataLoader(dataset=val_set, batch_size=BATCH_SIZE, shuffle=True)
-    len_val = len(val_set)
-
-    net = mspcnext_v1(num_classes=7,drop_path_rate=0.6).to("cuda")
-    loss_function = nn.CrossEntropyLoss()
-    optimizer = optim.AdaMod(net.parameters(), lr=0.1)
-    ema = ExponentialMovingAverage(net.parameters(), decay=0.995)
-
-    epoch = 150
-    mode = 'OneCycleLR'
-    max_epoch = 150
-    iters = int(len_train / BATCH_SIZE if len_train / BATCH_SIZE == 0 else len_train / BATCH_SIZE + 1)
-    print("max_epoch,iters", (max_epoch, iters))
-
-    if mode == 'cosineAnn':
-        multi_schedule = CosineAnnealingLR(optimizer, T_max=5, eta_min=0.001)
-    elif mode == 'cosineAnnWarm':
-        multi_schedule = CosineAnnealingWarmRestarts(optimizer, T_0=5, T_mult=2)
-    elif mode == 'OneCycleLR':
-        multi_schedule = OneCycleLR(optimizer, max_lr=0.003, total_steps=max_epoch * iters, pct_start=0.1,
-                                    div_factor=100000, final_div_factor=0.0003)
-
-    history = train_and_val(epoch, net, train_loader, len_train, val_loader, len_val, loss_function, optimizer,multi_schedule,device,ema)
-
-    plot_loss(np.arange(0, epoch), history)
-    plot_acc(np.arange(0, epoch), history)
+    inputs = torch.randn(1, 3, 224, 224)
+    model = mspcnext_v2()
+    print(model)
+    outputs = model(inputs)
+    print(outputs)
+    flops, params = profile(model, (inputs,))
+    print('flops: ', flops, 'params: ', params)
+    print("input dimension:", inputs.shape)
+    print("output dimension:", outputs.shape)
